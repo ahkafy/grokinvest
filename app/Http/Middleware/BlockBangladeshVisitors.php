ed1dc8e149b250ccf750487d896b48b4d42ef5b3
Abdullah hel kafy <ahkafy@gmail.com>
2025-08-15 17:25:01 +0600

    Restrict access to North America, South America, and Europe only via middleware

--- app/Http/Middleware/BlockBangladeshVisitors.php
+++ app/Http/Middleware/BlockBangladeshVisitors.php
@@ -10,23 +10,30 @@ class BlockBangladeshVisitors
     public function handle(Request $request, Closure $next)
     {
         $ip = $request->ip();
-        $continent = $this->getContinentFromIp($ip);
-        // Allow only North America (NA), South America (SA), Europe (EU)
-        $allowedContinents = ['NA', 'SA', 'EU'];
-        if (!in_array($continent, $allowedContinents)) {
+        $countryCode = $this->getCountryCodeFromIp($ip);
+        // List of allowed country codes for North America, South America, and Europe
+        $allowedCountries = [
+            // North America
+            'US','CA','MX','GL','BM','BS','BZ','CR','SV','GT','HN','NI','PA','AI','AG','AW','BB','BQ','VG','KY','CU','CW','DM','DO','GD','GP','HT','JM','MQ','MS','PR','BL','KN','LC','MF','PM','VC','SX','TT','TC','VI',
+            // South America
+            'AR','BO','BR','CL','CO','EC','FK','GF','GY','PY','PE','SR','UY','VE',
+            // Europe
+            'AL','AD','AM','AT','AZ','BY','BE','BA','BG','HR','CY','CZ','DK','EE','FO','FI','FR','GE','DE','GI','GR','HU','IS','IE','IT','KZ','XK','LV','LI','LT','LU','MT','MD','MC','ME','NL','MK','NO','PL','PT','RO','RU','SM','RS','SK','SI','ES','SJ','SE','CH','TR','UA','GB','VA'
+        ];
+        if (!in_array($countryCode, $allowedCountries)) {
             return response()->view('blocked-bd');
         }
         return $next($request);
     }
 
-    private function getContinentFromIp($ip)
+    private function getCountryCodeFromIp($ip)
     {
         // Use a free API for demonstration (ip-api.com)
         try {
-            $response = @file_get_contents("http://ip-api.com/json/{$ip}");
+            $response = @file_get_contents("http://ip-api.com/json/{$ip}?fields=countryCode");
             if ($response) {
                 $data = json_decode($response, true);
-                return $data['continentCode'] ?? null;
+                return $data['countryCode'] ?? null;
             }
         } catch (\Exception $e) {}
         return null;
